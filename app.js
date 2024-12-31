95b32c9020aeb73a77023f8467c9851c025487f5
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-12-30 17:07:27 -0800

    Add new paragraph

---
+++ app.js
@@ -0,0 +1,14 @@
+let direction = {x: 0, y: 0};
+const foodSound = new Audio('Assets/music/food.mp3');
+const gameOverSound = new Audio('Assets/music/gameover.mp3');
+const moveSound = new Audio('Assets/music/move.mp3');
+const musicSound = new Audio('Assets/music/music.mp3');
+
+// Game Functions
+function main(ctime) {
+    
+}
+
+
+// main logic starts here
+window.requestAnimationFrame(main);
