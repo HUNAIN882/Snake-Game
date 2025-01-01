8770c0b204cb878238a4a2b836ca0b051a91a1c9
HUNAIN882 <hunaintoufeeq@gmail.com>
2024-12-31 20:09:41 -0800

    Add new paragraph

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ let lastPaintTime = 0;
 let snakeArr = [
     {x: 13, y: 15}
 ]
-food = {x: 13, y: 15};
+food = {x: 6, y: 7};
 
 // Game Functions
 function main(ctime) {
     window.requestAnimationFrame(main);
-    console.log(ctime)
+    // console.log(ctime)
     if((ctime - lastPaintTime)/1000 < 1/speed){
         return;
     }
@@ -33,9 +33,13 @@ function gameEngine() {
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
         snakeElement.classList.add('snake');
-        if(index === 0) {
+        if(index == 0) {
             snakeElement.classList.add('head');
         }
+        else {
+            snakeElement.classList.add('snake');
+        }
+       
         board.appendChild(snakeElement);
     });
 
@@ -44,9 +48,40 @@ function gameEngine() {
     foodElement.style.gridRowStart = food.y;
     foodElement.style.gridColumnStart = food.x;
     foodElement.classList.add('food')
-    board.appendChild(snakeElement);
+    board.appendChild(foodElement);
 }
 
 
 // main logic starts here
 window.requestAnimationFrame(main);
+window.addEventListener('keydown', e => {
+    inputDir = {x: 0, y: 1} // Start the game
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+        break;
+
+        case "ArrowDown":
+        console.log("ArrowDown")
+        inputDir.x = 0;
+        inputDir.y = 1;
+        break;
+
+        case "ArrowLeft":
+            console.log("ArrowLeft")
+            inputDir.x = -1;
+            inputDir.x = 0;
+            break;
+
+            case "ArrowRight":
+                console.log("ArrowRight")
+                inputDir.y = 1;
+                inputDir.x = 0;
+                break;
+        default:
+            break;
+    }
+});
